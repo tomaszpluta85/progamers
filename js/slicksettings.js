65529b3ffa4f7c96485ef704c5ccc7696591d7dd
Tomasz Pluta <tomasz.pluta@developress.io>
2021-09-14 10:03:12 +0200

    progamers

---
+++ js/slicksettings.js
@@ -0,0 +1,19 @@
+$('.team-carousel').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    mobileFirst: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {slidesToShow: 2}
+        },
+        {
+            breakpoint: 992,
+            settings: {slidesToShow: 3}
+        }
+    ]
+});
